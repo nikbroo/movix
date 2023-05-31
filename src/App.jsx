5d38ea69caa3481b4c892a41782ed76d19d775e6
Nikhil <nikhilng.1997@gmail.com>
2023-05-31 21:47:20 +0530

    first_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { fechDataFromApi } from "./utils/api";
+import { fetchDataFromApi } from "./utils/api";
 
 import { useSelector, useDispatch } from 'react-redux';
 import { getApiConfiguration } from './store/homeSlice';
@@ -22,20 +22,28 @@ function App() {
   console.log(url)
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, [])
 
-  const apiTesting = () => {
-    fechDataFromApi('/movie/popular')
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration')
       .then((res) => {
         console.log(res);
-        dispatch(getApiConfiguration(res))
+
+        const url = {
+          backdrop: res.images.secure_base_url + "original",
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "original",
+        }
+
+        dispatch(getApiConfiguration(url))
       })
   }
 
   return (
     <BrowserRouter>
-      <Header />
+    {/* {url?.total_pages} */}
+      {/* <Header /> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/:mediaType/:id' element={<Details />} />
@@ -43,7 +51,7 @@ function App() {
         <Route path='/explore/:mediaType' element={<Explore />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   )
 }
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.scss'
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import useFetch from '../../../hooks/useFetch';
+
+import Img from '../../../components/lazyLoadImage/Img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 
 const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home)
+  const {data, loading} = useFetch("/movie/upcoming")
+
+  useEffect(() =>{
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data])
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      console.log("hellow");
+      navigate(`/search/${query}`);
+    }
+  };
+
   return (
     <div className='heroBanner'>
-        
+      {!loading && <div className='backdrop-img'>
+        <Img src={background} />
+      </div>}
+
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
+        <div className='heroBannerContent'>
+          <span className='title'>Welcome.</span>
+          <span className='subTitle'>
+            Millions of Movies, TV shows and people to discover.
+            Ecplore now.
+          </span>
+          <div className='searchInput'>
+            <input
+              type="text"
+              placeholder='Search for a movies or tv'
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+
+      
     </div>
-  )
-}
+  );
+};
 
 export default HeroBanner
