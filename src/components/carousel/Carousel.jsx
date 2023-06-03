a6192331aeb77b0427be65dd94e30650e8d03bb1
Nikhil <nikhilng.1997@gmail.com>
2023-06-03 23:14:34 +0530

    03-06

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,24 @@
+import React, { useRef } from "react";
+import {
+    BsFillArrowLeftCircleFill,
+    BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import Img from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
+
+import "./style.scss";
+
+const Carousel = ({data, loading}) => {
+    const carouselContainer = useRef();
+
+  return (
+    <div>Carousel</div>
+  )
+}
+
+export default Carousel
--- src/pages/home/tranding/Tranding.jsx
+++ src/pages/home/tranding/Tranding.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
+import Carousel from '../../../components/carousel/Carousel';
+
+
+import useFetch from "../../../hooks/useFetch";
+
 
 const Tranding = () => {
+  const [endpoint, setEndpoint] = useState("day")
 
-    const onTabChange = (tab) => {};
+  const {data, loading} = useFetch(`/trending/all/${endpoint}`);
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week" );
+  };
 
   return (
     <div className='carouselSection'>
-        <ContentWrapper>
-            <span className="carouselTitle">Tranding</span>
-            <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
-        </ContentWrapper>
+      <ContentWrapper>
+        <span className="carouselTitle">Tranding</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   )
 }
