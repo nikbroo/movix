f6ab1946edd2bfdba98cbb2d75d299d7812b7522
Nikhil <nikhilng.1997@gmail.com>
2023-11-01 12:10:01 +0530

    01_11_23

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.scss";
-import HeroBanner from "./heroBanner/HeroBanner"
+import HeroBanner from "./heroBanner/HeroBanner";
 
 const Home = () => {
-  return <div className="homePage">
-    <HeroBanner />
-  </div>;
+  return (
+    <div className="homePage">
+      <HeroBanner />
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+
+import useFetch from "../../../hooks/useFetch";
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
   return (
     <div className="heroBanner">
-      <div className="wrapper">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+
+      <div className="opacity-layer"></div>
+
+      <ContentWrapper>
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
           <span className="subTitle">
@@ -14,11 +47,14 @@ const HeroBanner = () => {
             <input
               type="text"
               placeholder="Search for a movie or tv show...."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
             />
             <button>Search</button>
           </div>
         </div>
-      </div>
+      </ContentWrapper>
+
     </div>
   );
 };
