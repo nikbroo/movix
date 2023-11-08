17ac761f311835dae0cc98a361cd3d968c2d6119
Nikhil <nikhilng.1997@gmail.com>
2023-11-08 23:33:17 +0530

    08_11_23

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,6 +8,7 @@ import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
+
 const Header = () => {
   const [show, setShow] = useState("top");
   const [lastScrollY, setLastScrollY] = useState(0);
@@ -16,6 +17,24 @@ const Header = () => {
   const [showSearch, setShowSearch] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
+
+  const controlNavbar = () => {
+    if(window.scrollY > 200) {
+      if(window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide")
+    } else {
+        setShow("show")
+      }
+    } else {
+      setShow("top")
+    }
+    setLastScrollY(window.scrollY)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar)
+    return () => {window.removeEventListener("scroll", controlNavbar)}
+  }, [lastScrollY])
 
   const searchQueryHandler = (event) => {
     if (event.key === "Enter" && query.length > 0) {
@@ -35,6 +54,14 @@ const Header = () => {
     setMobileMenu(true);
     setShowSearch(false);
   };
+  const navigationHandler = (type) => {
+    if(type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
+    setMobileMenu(false);
+  }
 
   return (
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
@@ -43,8 +70,8 @@ const Header = () => {
           <img src={logo} alt="" />
         </div>
         <ul className="menuItems">
-          <li className="menuItem">Movies</li>
-          <li className="menuItem">TV Shows</li>
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>Movies</li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>TV Shows</li>
           <li className="menuItem">
             <HiOutlineSearch onClick={openSearch} />
           </li>
